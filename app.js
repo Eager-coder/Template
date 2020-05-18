efb0fab55a29bc70d87de0fdac500ee89b617781
Turan Sultan <turan.sultann@gmail.com>
2020-05-18 22:33:57 +0600

    commit

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-const backgrounds = Array.from(document.querySelectorAll('.image'));
 
+// Background effects
+const backgrounds = Array.from(document.querySelectorAll('.image'));
 backgrounds.forEach((e, index)=>{
    e.style.background = `linear-gradient(90deg, rgba(0, 0, 0, 0.445), rgba(0, 126, 243, 0.6)), url(../images/g${index+1}.jpg)`;
    e.style.backgroundPosition = 'top';
    e.style.backgroundSize = 'cover';
    e.parentElement.addEventListener('mouseover', ()=>{
-       e.style.backgroundPosition = 'center';
+       e.style.backgroundPosition = 'right 50% top 50%';
    })
    e.parentElement.addEventListener('mouseout', ()=>{
     e.style.backgroundPosition = 'top';
 })
 })
+
+
+// Video in the background
+const videoContainer = document.querySelector('.video-section');
+const video = document.createElement('video');
+video.src = './videos/meeting.mp4';
+videoContainer.appendChild(video);
+window.addEventListener('load', ()=>{
+    video.play();
+    video.loop = true ;
+} );
 
