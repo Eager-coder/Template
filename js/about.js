0ca09c4c573bbff2757ec4fdc35a9acd29c91736
Turan Sultan <turan.sultann@gmail.com>
2020-05-22 22:16:13 +0600

    commit

--- js/about.js
+++ js/about.js
@@ -1,3 +1,16 @@
 import {openNav} from './common.js';
 // Navigation 
 openNav();
+
+// Carousel
+const carousel = document.querySelector('.carousel-container');
+const carouselItem = carousel.querySelector('.member');
+const prevBtn = document.querySelector('.chevron-left');
+const nextBtn = document.querySelector('.chevron-right');
+const getHeight = function(){
+    return carouselItem.clientWidth 
+};
+// setInterval(getHeight, 500);
+
+prevBtn.addEventListener('click', ()=> carousel.scrollLeft -= getHeight())
+nextBtn.addEventListener('click', ()=> carousel.scrollLeft += getHeight())
